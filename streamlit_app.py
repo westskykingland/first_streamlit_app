bc6667a2629cc25413fb6cbe8cb9fedd2127051e
westskykingland <42556971+westskykingland@users.noreply.github.com>
2022-11-27 13:19:11 -0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,7 @@
+
 import streamlit
+streamlit.title("my parent's new healthy dinner");
+
 streamlit.header('Breakfast Menu')
 streamlit.text('Omega 3 & Blueberry Oatmeal')
 streamlit.text('Kale, Spinach & Rocket Smoothie')
